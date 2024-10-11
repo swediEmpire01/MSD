33869392d481d05da81e5a661f7f1e5eb3faff03
Husseni Daniel Swedi <151975571+swediEmpire01@users.noreply.github.com>
2024-10-11 19:05:40 -0400

    Added links to the buttons of the page and capitalized the title and About me section

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function sendEmail() {
     const message= document.getElementById("mmessage").value;
     const name= document.getElementById("name").value + " " + document.getElementById("surename").value;
     const contact= document.getElementById("contact").value;
-    
+
     emailjs.send("service_zqmy47w","template_ttscy6c",{
       to_name: "MorningStudio",
       from_name:  `${name}  :  ${contact}`,
@@ -45,3 +45,12 @@ function sendEmail() {
         },
       );;
   }
+
+// code used to go to whatsapp when a buttin in the merch section is clicked
+const merchSection= document.getElementById("merch");
+
+merchSection.addEventListener("click", e=>{
+  if(e.target.tagName === "BUTTON"){
+    window.open("https://wa.me/message/6R6Z7XZ2D7I6N1");
+  }
+})
